70802e08cb5c6e54cabc9a50d3229ff6547babe2
Bastards <diego.esgu@gmail.com>
2019-09-17 20:18:09 -0500

    tips

--- src/main/java/com/diesgut/jpa/sakila/MainConPersistenceXML.java
+++ src/main/java/com/diesgut/jpa/sakila/MainConPersistenceXML.java
@@ -30,6 +30,10 @@ public class MainConPersistenceXML {
         //En una transacción jpa primero ejecuta los insert, luego los update y finalmente los deletes (sin importar el orden como estén las operaciones en el codigo).
         //a menos que vallamos haciendo flush luego de cada operación
         //las operacions find y getResultList hacen flush automaticamente
+        //flush : recorre todos los objetos del persistence context y validar si se requiere enviar cambios a la base de datos
+        //al trabajar con listas grandes, lo dieal es ir limpiando esos objetos de persistence context con el metodo clear() del entitymanager (bastante recomendable)
+        //colocando readOnley=true en nuestra transaccion deshabilita el flush automatico y el resultado sera similar a ir limpiando el contexto
+        //no es recomendable hacer un proceso de larga duracion (webservice o procesar datos) en una transaccion, puesto que tomara una conexion por mucho tiempo, evitando el acceso a la applicacion
         em.getTransaction().commit();
         em.close();
         emf.close(); //cierra el pool de conexiones (proceso separado de nuestor proceso principal (main))
